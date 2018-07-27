d2bcb97d5f20b2345e26056b6bf049b7e80c4886
MOHANRAJ D <mohanrajajantha@gmail.com>
2018-07-27 15:21:24 +0530

    Update MainClass.java

--- InterestCalculation/com.wipro.bank.main/MainClass.java
+++ InterestCalculation/com.wipro.bank.main/MainClass.java
@@ -8,9 +8,13 @@ public class MainClass {
 	public static void main(String[] args)
 	{
 		Scanner input=new Scanner(System.in);
+		System.out.println("Enter tenure:");
 		int tenure=input.nextInt();
+		System.out.println("Enter principal:");
 		float principal=input.nextFloat();
+		System.out.println("Enter age:");
 		int age=input.nextInt();
+		System.out.println("Enter gender:");
 		String gender=input.next();
 		gender=gender.toLowerCase();
 		BankService b=new BankService();
